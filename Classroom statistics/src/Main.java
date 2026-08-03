f43d61f5d7e10285446d99409a4fe9cd1ab2c646
shamssamirr00 <shamsh.farahat@gmail.com>
2026-08-04 01:20:38 +0300

    Add problem 17 : "About Classroom statistics"

---
+++ Classroom statistics/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double Highest_Mark = 0;
+        double Lowest_Mark = 100 ;
+        int sum = 0;
+        int passed = 0;
+        for (int i = 1 ; i <= 10 ; i++) {
+            System.out.print("Enter Mark " + (i) + " = ");
+            double mark = input.nextInt();
+            if (mark > Highest_Mark) {
+                Highest_Mark = mark;
+            }
+            if (mark < Lowest_Mark) {
+                Lowest_Mark = mark;
+            }
+            sum += mark;
+            if (mark >= 6-) {
+                passed++;
+            }
+        }
+        System.out.println("Highest Mark: " + Highest_Mark);
+        System.out.println("Lowest Mark: " + Lowest_Mark);
+        System.out.println("Average Mark: " + sum/10.0);
+        System.out.println("Passed: " + passed);
+    }
+}
